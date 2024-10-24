4e6d8142a30cbd38b483ae9ee16113d455d6d7c2
Francisco <frdelgado@alumnos.uai.cl>
2024-10-24 20:09:24 -0300

    Primer commit del nuevo trabajo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+
+        <Routes>
+
+        </Routes>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;

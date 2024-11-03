33cc80b27f1b5a3512dceffe476f5d9a46db758d
ZetaBeeM <thisnameisnotpermitted@gmail.com>
2024-11-03 17:29:47 -0300

    Añadido funcionalidad de los botones para cambiar idiomas en todas las paginas (falta traducir el texto al inglés)

--- src/paginas/Educacion.js
+++ src/paginas/Educacion.js
@@ -15,81 +15,81 @@ const Educacion = () => {
     const cursos = [
         {
           id: 1,
-          nombre: 'Bono Logro Escolar',
-          descripcion: 'Es un beneficio al cual no se postula y que forma parte del Ingreso Ético Familiar (IEF). Está destinado a estudiantes menores de 24 años, de entre quinto básico y cuarto medio, que pertenezcan al 30% de mejor rendimiento académico de su promoción, y cuyas familias pertenezcan al 30% más vulnerable de la población chilena.',
+          nombre: t('curso1.name'),
+          descripcion: t('curso1.description'),
           requisitos: [
-            'Que sus familias sean beneficiarias del subsistema Seguridades y Oportunidades del Ingreso Ético Familiar (IEF).',
-            'Estar dentro del 30% más vulnerable de la población según al Registro Social de Hogares (RSH)., al 31 de marzo del año inmediatamente anterior a aquel en que se pague el Bono. Atención: Si tu cartola hogar indica que estás dentro del 40%, no implica necesariamente que estés dentro del 30% más vulnerable.',
-            'Cursar entre 5º básico y 4º medio.',
-            'Estar en el 30% de mejor rendimiento académico de su promoción o grupo de egreso durante el año anterior al que se postula.',
-            'Haber asistido a establecimientos de enseñanza básica o media reconocidos por el Ministerio de Educación (Mineduc).'
+            t('curso1.requisito1'),
+            t('curso1.requisito2'),
+            t('curso1.requisito3'),
+            t('curso1.requisito4'),
+            t('curso1.requisito5')
           ]
         },
         {
             id: 2,
-            nombre: 'Beca Práctica Técnico Profesional',
-            descripcion: 'La Beca Práctica Técnico Profesional (BPTP) es un beneficio de 65 mil pesos de libre disposición o uso, pagados en una cuota, a estudiantes o egresados/as de enseñanza media técnico-profesional, que estén realizando o vayan a iniciar su práctica profesional dentro del año en curso.',
+            nombre: t('curso2.name'),
+            descripcion: t('curso2.description'),
             requisitos: [
-              'Ser estudiante o egresado/a de un establecimiento educacional de Enseñanza Media Técnico Profesional.',
-              'Debes realizar o iniciar la práctica profesional durante el año en curso.',
-              'Puedes postular si iniciaste tu práctica el año anterior y el proceso continúa durante el año en curso.',
-              'Presentar un informe de práctica del establecimiento educacional (completar anexo Nº 10).',
-              '*** Importante: quienes postulen no deben haber sido beneficiarios/as de la bonificación en años anteriores.'
+              t('curso2.requisito1'),
+              t('curso2.requisito2'),
+              t('curso2.requisito3'),
+              t('curso2.requisito4'),
+              t('curso2.requisito5')
             ]
           },
         {
             id: 3,
-            nombre: 'Beca de Integración Territorial - Educación Media',
-            descripcion: 'Es un aporte económico destinado a cubrir parte de los gastos de mantención y transporte para estudiantes de educación media de las provincias de Coyhaique, Aysén, General Carrera, Capitán Prat, Isla de Pascua, Palena, Última Esperanza, Magallanes, Tierra del Fuego, Antártica Chilena y en la comuna de Juan Fernández. El monto del beneficio corresponde a 18,7 UTM anuales, las que se pagan en 10 cuotas mensuales.',
+            nombre: t('curso3.name'),
+            descripcion: t('curso3.description'),
             requisitos: [
-              'Tener domicilio en la Región de Aysén o de Magallanes, en las provincias de Palena, o en las comunas de Isla de Pascua o Juan Fernández',
-              'Necesitas trasladarte y vivir fuera de las localidades en las que resides para poder realizar tus estudios.',
-              'Debes estudiar en establecimientos educacionales reconocidos por el Estado que no tengan oferta en tu localidad de residencia.',
-              'Debes estar dentro del 80% más vulnerable según el Registro Social de Hogares (RSH) .',
-              'Necesitas tener un promedio mínimo de notas de 5,0 (Se exime de este requisito a estudiantes de Educación Especial)',
-              'Si estás en el Registro Nacional de Discapacidad (RND) tendrás acceso preferente'
+              t('curso3.requisito1'),
+              t('curso3.requisito2'),
+              t('curso3.requisito3'),
+              t('curso3.requisito4'),
+              t('curso3.requisito5'),
+              t('curso3.requisito6')
             ]
           },
         {
             id: 4,
-            nombre: 'Beca Polimetales - Educación Media',
-            descripcion: 'Es un aporte económico de libre disposición dirigido a estudiantes afectados por la contaminación de las zonas con presencia de polimetales de la comuna de Arica, que se encuentren estudiando entre primero y cuarto medio en establecimientos educacionales reconocidos por el Estado. El beneficio corresponde a 6,2 UTM anuales y se paga en hasta 10 cuotas mensuales. ',
+            nombre: t('curso4.name'),
+            descripcion: t('curso4.description'),
             requisitos: [
-              'Estar matriculado en algún establecimiento educacional de educación media reconocido por el Estado.',
-              'Acreditar la condición de afectado por la contaminación de polimetales de la comuna de Arica.',
-              'Certificado que acredita la condición de afectado por la contaminación de polimetales.',
-              'Certificado de matrícula o de alumno regular que indique su curso en un establecimiento reconocido por el Estado.',
-              'En caso de renovación, necesitarás también el certificado de notas que acredite la aprobación del año académico anterior.'
+              t('curso4.requisito1'),
+              t('curso4.requisito2'),
+              t('curso4.requisito3'),
+              t('curso4.requisito4'),
+              t('curso4.requisito5'),
             ]
           },
         {
             id: 5,
-            nombre: 'Programa Residencia Familiar Estudiantil (PRFE) – Educación media',
-            descripcion: 'Es un beneficio que entrega alojamiento en hogares de familias tutoras a estudiantes que deben trasladarse de su comuna de residencia para continuar sus estudios de educación media. También consiste en apoyo de alimentación y en el ámbito pedagógico para facilitar la continuación y la finalización de los estudios de educación media. ',
+            nombre: t('curso5.name'),
+            descripcion: t('curso5.description'),
             requisitos: [
-              'Tienes que residir en comunas que tengan al menos un 50% de ruralidad.',
-              'Debes ser estudiante de algún establecimiento particular subvencionado de Enseñanza Media.',
-              'Tienes que estar inscrito/a en el Registro Social de Hogares (RSH)',
+              t('curso5.requisito1'),
+              t('curso5.requisito2'),
+              t('curso5.requisito3'),
             ]
           },
         {
             id: 6,
-            nombre: 'Programa Residencia Familiar Estudiantil (PRFE) – Educación superior',
-            descripcion: 'Es un beneficio que entrega alojamiento en hogares de familias tutoras a estudiantes que deben trasladarse de su comuna de residencia para continuar sus estudios superiores. También consiste en apoyo de alimentación y en el ámbito pedagógico para facilitar la continuación y la finalización de los estudios de educación superior.',
+            nombre: t('curso6.name'),
+            descripcion: t('curso6.description'),
             requisitos: [
-              'Tienes que residir en comunas que tengan al menos un 50% de ruralidad.',
-              'Debes ser estudiante en alguna Institución de Educación Superior reconocida por el Estado.',
-              'Tienes que estar inscrito/a en el Registro Social de Hogares (RSH). ',
+              t('curso6.requisito1'),
+              t('curso6.requisito2'),
+              t('curso6.requisito3'),
             ]
           },
           {
             id: 7,
-            nombre: 'Beca Residencia Insular (BRINS)',
-            descripcion: 'Es un aporte económico destinado a cubrir parte de los gastos de arriendo para estudiantes que residen en Isla Juan Fernández e Isla de Pascua y que estudian en instituciones de educación superior reconocidas por el Estado. El monto del beneficio corresponde a $981.580 y se paga hasta en 10 cuotas al año.',
+            nombre: t('curso7.name'),
+            descripcion: t('curso7.description'),
             requisitos: [
-              'Tienes que residir en la Isla Juan Fernández o en la Isla de Pascua.',
-              'Debes estar matriculado/a en una institución de Educación Superior reconocidas por el Estado.',
-              'Tienes que estar inscrito/a en el Registro Social de Hogares (RSH).',
+              t('curso7.requisito1'),
+              t('curso7.requisito2'),
+              t('curso7.requisito3'),
             ]
           }
 
@@ -98,7 +98,7 @@ const Educacion = () => {
     
       return (
         <div className="container educacion-bg mt-5"> 
-            <h1 className="text-center">Cursos de Educación Media</h1>
+            <h1 className="text-center">{t('cursos')}</h1>
             <button onClick={toggleLanguage} className="btn btn-secondary mb-3">
             {i18n.language === 'es' ? 'Switch to English' : 'Cambiar a Español'}
           </button>
--- src/paginas/Pensiones.js
+++ src/paginas/Pensiones.js
@@ -15,92 +15,92 @@ const Penciones = () => {
     const retribuciones = [
         {
           id: 1,
-          nombre: 'Pensión Garantizada Universal',
-          descripcion: 'La Pensión Garantizada Universal (PGU) es un beneficio que reemplazó las pensiones del pilar solidario de vejez (tanto la Pensión Básica Solidaria-PBS como el Aporte Previsional Solidario-APS). Desde febrero del 2024, el monto de la PGU es de $214.296 pesos mensuales*. Este monto se suma a la pensión autofinanciada con los ahorros previsionales (AFP). Desde febrero de 2022, las personas beneficiarias de una pensión del Pilar Solidario reciben automáticamente la PGU (exceptuando algunos casos, detallados más abajo). Desde agosto del mismo año, la PGU beneficia a todas las personas mayores de 65 años que pertenezcan al 90% más vulnerable**. El beneficio se entrega independientemente de que las personas mayores ya se hayan pensionado o sigan trabajando. Para recibir la PGU, tu pensión autofinanciada debe ser inferior a $1.158.355 mensuales. En caso de que tu pensión autofinanciada sea superior a $729.764, recibirás una fracción del monto total de la PGU (puedes revisar el detalle de estos montos más abajo). Si aún no recibes el beneficio y cumples con todos los requisitos debes solicitarlo.',
+          nombre: t('pension1.name'),
+          descripcion: t('pension1.description'),
           requisitos: [
-            'Tener 65 años o más.',
-            'No integrar un hogar del 10% más rico de la población mayor de 65 años.',
-            'Tener residencia en Chile por al menos 20 años (continuos o discontinuos) desde los 20 años de la persona que solicita el beneficio. Además debe haber residido en Chile al menos cuatro de los cinco últimos años previo a la solicitud.',
-            'No tener derecho a pensión o contar con una pensión base menor a la pensión superior ($1.158.355).',
-            'No estar afiliado en DIPRECA o CAPREDENA.'
+            t('pension1.requisito1'),
+            t('pension1.requisito2'),
+            t('pension1.requisito3'),
+            t('pension1.requisito4'),
+            t('pension1.requisito5')
           ]
         },
         {
             id: 2,
-            nombre: 'Pensión Básica Solidaria de Invalidez (PBSI)',
-            descripcion: 'Es un beneficio económico mensual para personas que puedan ser declaradas con invalidez*, que no tengan derecho a una pensión en algún régimen previsional y que pertenecen al 80% más vulnerable de la población. El monto de este beneficio es de máximo $214.296. Este valor se reajustará todos los febrero desde 2023.',
+            nombre: t('pension2.name'),
+            descripcion: t('pension2.description'),
             requisitos: [
-              'Debes tener entre 18 y 64 años al momento de la solicitud.',
-              'No debes recibir pensión en ningún régimen previsional, ya sea en calidad de titular o como beneficiario de una pensión de sobrevivencia.',
-              'Debes ser declarado o declarada con invalidez por las Comisiones Médicas correspondientes, definidas en el Decreto Ley N° 3.500.',
-              'Que integren un grupo familiar perteneciente al 80% más vulnerable de la población según el Registro Social de Hogares (RSH).',
-              'Haber vivido en Chile por, al menos, cinco de los últimos seis años anteriores a la fecha en que ingresas tu solicitud. Estos cinco años pueden ser continuos o discontinuos*.',
-              'No debes ausentarte del país por un período mayor a 90 días durante el año calendario (de enero a diciembre).',
-              'Tener tu cédula de identidad vigente.'
+              t('pension2.requisito1'),
+              t('pension2.requisito2'),
+              t('pension2.requisito3'),
+              t('pension2.requisito4'),
+              t('pension2.requisito5'),
+              t('pension2.requisito6'),
+              t('pension2.requisito7')
             ]
           },
         {
             id: 3,
-            nombre: 'Aporte Previsional Solidario de Invalidez (APSI)',
-            descripcion: 'Es un beneficio económico mensual para las personas que reciben pensiones de invalidez* de bajo monto, es decir, para personas que reciben pensiones menores a la Pensión Garantizada Universal (PGU) $214.296. Estas pensiones son destinadas a personas que no puede llevar a cabo un trabajo, ya sea por condiciones físicas o por alguna discapacidad intelectual. Esta condición la declaran las Comisiones Médicas correspondientes, definidas en el Decreto Ley N° 3.500.',
+            nombre: t('pension3.name'),
+            descripcion: t('pension3.description'),
             requisitos: [
-              'Tener entre 18 y 64 años al momento de la solicitud.',
-              'Recibir una pensión de invalidez mensual o una pensión base inferior al monto de la Pensión Básica Solidaria.',
-              'haber sido declarado o declarada con invalidez por las Comisiones Médicas correspondientes, definidas en el Decreto Ley N° 3.500.',
-              'Que integren un grupo familiar perteneciente al 80% más pobre de la población según el Registro Social de Hogares (RSH).',
-              'Haber vivido en Chile por al menos cinco de los últimos seis años anteriores a la fecha en que ingresas tu solicitud. Estos 5 años pueden ser continuos o discontinuos.',
-              'No debes cotizar en la Dirección de Previsión de Carabineros de Chile (DIPRECA), ni en la Caja de Previsión de la Defensa Nacional (CAPREDENA).',
-              'No debes ausentarte del país por un período mayor a 90 días durante el año calendario (de enero a diciembre).',
-              'Tener tu cédula de identidad vigente.'
+              t('pension3.requisito1'),
+              t('pension3.requisito2'),
+              t('pension3.requisito3'),
+              t('pension3.requisito4'),
+              t('pension3.requisito5'),
+              t('pension3.requisito6'),
+              t('pension3.requisito7'),
+              t('pension3.requisito8')
             ]
           },
         {
             id: 4,
-            nombre: 'Aguinaldo por Fiestas Patrias para Pensionados/as',
-            descripcion: 'Se trata de un beneficio que se paga en septiembre de cada año, a todas las personas beneficiarias de pensiones que cumplan con los requisitos establecidos. El monto del aguinaldo el 2024 es de $24.261. A eso se le suman $12.446 por cada carga familiar que esté acreditada al 31 de agosto de 2024',
+            nombre: t('pension4.name'),
+            descripcion: t('pension4.description'),
             requisitos: [
-              'Pensionados/as de las ex-cajas de previsión.',
-              'Pensionados/as de las leyes de Exonerados/as Políticos/as, Ley 19.234.',
-              'Pensionados/as de la Ley de Reparación 19.123 (Ley Rettig).',
-              'Pensionados/as de la Ley de Reparación 19.992 (Ley Valech).',
-              'Pensionados/as de Accidentes del Trabajo, Ley 16.744, del Instituto de Seguridad Laboral.',
-              'Beneficiarios/as de la Pensión Garantizada Universal.',
-              'Beneficiarios/as de la Pensión Básica Solidaria de Invalidez',
-              'Personas que reciben Subsidio por Discapacidad Mental, Art. 35 de la Ley N° 20.255.',
-              'Beneficiarios/as de Indemnizaciones del Carbón, Ley N° 19.129.'
+              t('pension4.requisito1'),
+              t('pension4.requisito2'),
+              t('pension4.requisito3'),
+              t('pension4.requisito4'),
+              t('pension4.requisito5'),
+              t('pension4.requisito6'),
+              t('pension4.requisito7'),
+              t('pension4.requisito8'),
+              t('pension4.requisito9')
             ]
           },
         {
             id: 5,
-            nombre: 'Aguinaldo de Navidad para Pensionados/as',
-            descripcion: 'Se trata de un beneficio que se paga en diciembre de cada año a las personas pensionadas al 30 de noviembre y que cumplan los requisitos. La entrega de este aguinaldo se hace junto al pago de la pensión de diciembre. El monto del aguinaldo este 2023 es de $26.734, y se puede incrementar en $15.104 por cada carga familiar.',
+            nombre: t('pension5.name'),
+            descripcion: t('pension5.description'),
             requisitos: [
-              'Beneficiarios/as de la Pensión Garantizada Universal o de la Pensión Básica Solidaria de Invalidez (PBSI)',
-              'Pensionados/as del Instituto de Seguridad Laboral (ISL).',
-              'Pensionados/as de las ex Cajas de Previsión y ex Servicio de Seguro Social (INP, actualmente IPS).',
-              'Pensionados/as de Caja de Previsión de Dipreca y Capredena.',
-              'Pensionados/as de las Mutualidades de Empleadores de la Ley Nº 16.744.',
-              'Pensionados/as de reparación: Rettig y Valech.',
-              'Pensionados/as de AFP o compañías de seguro con garantía estatal o Aporte Previsional Solidario (de Vejez o de Invalidez).',
-              'Beneficiarios/as de la indemnización compensatoria especial para la industria del carbón.',
-              'Beneficiarios/as del Subsidio por Discapacidad Mental.'
+              t('pension5.requisito1'),
+              t('pension5.requisito2'),
+              t('pension5.requisito3'),
+              t('pension5.requisito4'),
+              t('pension5.requisito5'),
+              t('pension5.requisito6'),
+              t('pension5.requisito7'),
+              t('pension5.requisito8'),
+              t('pension5.requisito9')
             ]
           },
         {
             id: 6,
-            nombre: 'Pensión de Sobrevivencia del Sistema de Pensiones (AFP)',
-            descripcion: 'Permite a personas beneficiarias de imponentes y pensionados/as que fallecieron y estaban afiliados/as al sistema de Administradoras de Fondos de Pensiones (AFP) solicitar la pensión de sobrevivencia.',
+            nombre: t('pension6.name'),
+            descripcion: t('pension6.description'),
             requisitos: [
-              '50% para la o el cónyuge con hijos/as comunes.',
-              '60% para la o el cónyuge si no existen hijos/as en común.',
-              '60% para la o el cónyuge sin hijos/as en común o hijos/as solamente de la o el causante.',
-              '50% para la o el conviviente civil con hijos/as en común.',
-              '15% para la o el conviviente civil si es que existen hijos/as de la o el causante y no hay hijos/as en común.',
-              '36% para la madre y/o padre de hijos/as no matrimoniales que no tienen derecho a pensión.',
-              '30% para la madre y/o padre de hijos/as que tienen derecho a pensión.',
-              '15% para hijos/as que cumplan los requisitos (ver campo "Beneficiarios/as").',
-              '11% para hijos/as con discapacidad parcial que cumplan 24 años.',
-              '50% para las madres y padres de la o el causante que sean beneficiarios/as de asignación familiar.'
+              t('pension6.requisito1'),
+              t('pension6.requisito2'),
+              t('pension6.requisito3'),
+              t('pension6.requisito4'),
+              t('pension6.requisito5'),
+              t('pension6.requisito6'),
+              t('pension6.requisito7'),
+              t('pension6.requisito8'),
+              t('pension6.requisito9'),
+              t('pension6.requisito10')
             ]
           },
 
@@ -109,7 +109,7 @@ const Penciones = () => {
     
       return (
         <div className="container benficios-bg mt-5"> 
-            <h1 className="text-center">Pensiones para el ciudadano</h1>
+            <h1 className="text-center">{t('pensiones')}</h1>
             <button onClick={toggleLanguage} className="btn btn-secondary mb-3">
             {i18n.language === 'es' ? 'Switch to English' : 'Cambiar a Español'}</button>
             <div className="row d-flex flex-wrap justify-content-center"> 
